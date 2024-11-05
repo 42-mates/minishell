89fd3f7f016a8497d3fcea3368bd7bf256809a1e
prosviriakova <ol7897@ya.ru>
2024-11-05 22:18:54 +0100

    sigint debugging, norms

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 11:40:34 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/11/05 20:52:02 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/11/05 22:17:40 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,44 +40,45 @@ static char	*generate_prompt(void)
 	return (prompt);
 }
 
-void free_command(t_command *cmd)
+void	free_command(t_command *cmd)
 {
-    int i = 0;
+	int	i;
 
-    if (!cmd)
-        return;
-    while (cmd->args[i])
-    {
-        free(cmd->args[i]);
-        i++;
-    }
-    free(cmd->args);
-    free(cmd);
+	i = 0;
+	if (!cmd)
+		return ;
+	while (cmd->args[i])
+	{
+		free(cmd->args[i]);
+		i++;
+	}
+	free(cmd->args);
+	free(cmd);
 }
 
 // for debugging
-void print_command(t_command *cmd)
+void	print_command(t_command *cmd)
 {
-    int i;
+	int	i;
 
-    if (!cmd)
-    {
-        printf("Command is NULL\n");
-        return;
-    }
-    printf("Command name: %s\n", cmd->name);
-    printf("Arguments:\n");
-    if (cmd->args)
-    {
-        i = 0;
-        while (cmd->args[i])
-        {
-            printf("  args[%d]: %s\n", i, cmd->args[i]);
-            i++;
-        }
-    }
-    else
-        printf("  No arguments\n");
+	if (!cmd)
+	{
+		printf("Command is NULL\n");
+		return ;
+	}
+	printf("Command name: %s\n", cmd->name);
+	printf("Arguments:\n");
+	if (cmd->args)
+	{
+		i = 0;
+		while (cmd->args[i])
+		{
+			printf("  args[%d]: %s\n", i, cmd->args[i]);
+			i++;
+		}
+	}
+	else
+		printf("  No arguments\n");
 }
 
 static int	init_shell(int argc, char **argv)
@@ -97,12 +98,12 @@ static int	init_shell(int argc, char **argv)
 
 static void	minishell(char *line)
 {
-	t_command *cmd;
+	t_command	*cmd;
 
 	cmd = parser(line);
 	print_command(cmd);
-	//executor(cmd);
-	free_command(cmd);	
+	// executor(cmd);
+	free_command(cmd);
 }
 
 int	main(int argc, char *argv[])
--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 20:36:13 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/11/05 21:06:08 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/11/05 22:16:59 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 static void	handle_sigint(int sig)
 {
 	(void)sig;
-    write(STDOUT_FILENO, "\n^C", 3);
+	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
 // EOF (ctrl-D)
-void    handle_eof(void)
+void	handle_eof(void)
 {
-    printf("exit\n");
-    exit(EXIT_SUCCESS);
+	printf("exit\n");
+	exit(EXIT_SUCCESS);
 }
 
 void	setup_signals(void)
