fb7bcbaf2a0ed174b0ddf13dc96825c58659ce79
Olga Prosviriakova <94167082+prosviriakova@users.noreply.github.com>
2024-11-10 22:51:42 +0000

    initial work on the executor

--- src/executor.c
+++ src/executor.c
@@ -6,13 +6,32 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 11:44:54 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/11/10 21:05:14 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/11/10 22:34:33 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	executor(t_command *cmd, t_env *env_list)
+// note : заглушка для начала работы executor + pwd, echo
+static int is_builtin(const char *cmd_name)
+{
+    const char *builtins[] = {"cd", "export", "unset", "env", "exit", NULL};
+    for (int i = 0; builtins[i]; i++)
+	{
+        if (ft_strcmp(cmd_name, builtins[i]) == 0)
+            return 1;
+    }
+    return 0;
+}
+
+// note : нужна реализация
+void execute_builtin(t_command *cmd, t_env *env_list)
+{
+	(void)cmd;
+	(void)env_list;
+}
+
+void	execute_command(t_command *cmd, t_env *env_list)
 {
 	pid_t	pid;
 	int		status;
@@ -37,4 +56,12 @@ void	executor(t_command *cmd, t_env *env_list)
 		}
 		while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
+}
+ 
+void	executor(t_command *cmd, t_env *env_list)
+{
+	if (is_builtin(cmd->name))
+		execute_builtin(cmd, env_list);
+    else
+		execute_command(cmd, env_list);
 }
