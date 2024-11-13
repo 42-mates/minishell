08b17cfb735f4f8e5f479ce7724d70bf1fd9e7f2
Olga Prosviriakova <94167082+prosviriakova@users.noreply.github.com>
2024-11-13 22:44:59 +0000

    small fix builtin

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 21:28:03 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/11/13 21:28:49 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/11/13 22:39:25 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ void	ft_exit(t_command *cmd, t_shell *shell)
 // note : нужна реализация
 void	execute_builtin(t_command *cmd, t_shell *shell)
 {
-	if (ft_strncmp(cmd->name, "exit", 5) == 0)
+	if (ft_strcmp(cmd->name, "exit") == 0)
 		ft_exit(cmd, shell);
 }
