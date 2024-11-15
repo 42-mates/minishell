8e3459d2fb54e105d4f4340c7e8590c004622b30
Marina Glikenfreid <mglikenf@made-f0Cr6s6.clusters.42paris.fr>
2024-11-15 15:16:30 +0100

    added 42 header

--- src/echo.c
+++ src/echo.c
@@ -1,4 +1,14 @@
-// add header
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mglikenf <mglikenf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 15:12:35 by mglikenf          #+#    #+#             */
+/*   Updated: 2024/11/15 15:12:46 by mglikenf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
 
@@ -16,7 +26,8 @@ void    ft_echo(t_command *cmd, t_shell *shell)
         while (cmd->args[i] != NULL)
         {
             ft_putstr_fd(cmd->args[i], 1);
-            ft_putstr_fd(" ", 1);
+            if (cmd->args[i + 1] != NULL)
+                ft_putstr_fd(" ", 1);
             i++;
         }
         if (ft_strcmp(cmd->args[1], "-n") != 0)
