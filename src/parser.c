da1e6e40a46cc00d82f841c308dfe211d301bfec
Olga Prosviriakova <94167082+prosviriakova@users.noreply.github.com>
2024-11-17 22:42:33 +0000

    feat : simple parser for executor debugging

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 11:42:50 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/11/17 19:04:43 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/11/17 22:30:28 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,65 @@ void handle_pipes(char **commands)
 }
 */
 
+t_command *init_command(void)
+{
+    t_command *cmd;
+
+    cmd = malloc(sizeof(t_command));
+    if (!cmd)
+    {
+        ft_printf("allocation error\n");
+        return (NULL);
+    }
+    cmd->name = NULL;
+    cmd->args = NULL;
+    cmd->output_file = NULL;
+    cmd->input_file = NULL;
+    cmd->next = NULL;
+    return cmd;
+}
+
+t_command *parser(char *line, t_shell *shell)
+{
+    char **commands;
+    t_command *head = NULL;
+    t_command *current = NULL;
+    int i = 0;
+
+    commands = ft_split(line, '|'); // Разделяем команды по '|'
+    while (commands[i])
+    {
+        t_command *new_cmd = init_command();
+        new_cmd->args = ft_strtok(commands[i], TOKEN_DELIM);
+        new_cmd->name = new_cmd->args[0];
+
+        if (!is_builtin(new_cmd->name))
+        {
+            char *exec_path = get_path(new_cmd, shell->env_vars);
+            if (!exec_path)
+            {
+                ft_putstr_fd(new_cmd->name, 2);
+                ft_putstr_fd(": command not found\n", 2);
+                shell->exit_status = 127;
+                free(new_cmd);
+                continue;
+            }
+            new_cmd->name = exec_path;
+        }
+
+        if (!head)
+            head = new_cmd;
+        else
+            current->next = new_cmd;
+
+        current = new_cmd;
+        i++;
+    }
+    free_memory(commands);
+    return head;
+}
+
+/*
 t_command	*parser(char *line, t_shell *shell)
 {
 	char		**tokens;
@@ -41,12 +100,7 @@ t_command	*parser(char *line, t_shell *shell)
 		free(tokens);
 		return (NULL);
 	}
-	cmd = malloc(sizeof(t_command));
-	if (!cmd)
-	{
-		ft_printf("allocation error\n");
-		exit(EXIT_FAILURE);
-	}
+	cmd = init_command();
 	cmd->name = tokens[0];
 	cmd->args = tokens;
 	if (!is_builtin(cmd->name))
@@ -66,4 +120,4 @@ t_command	*parser(char *line, t_shell *shell)
 		cmd->args = tokens;
 	}
 	return (cmd);
-}
+}*/
