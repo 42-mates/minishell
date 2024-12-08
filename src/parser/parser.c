d1a6be55a5502594fbef5f510dc885e4175c0775
Olga Prosviriakova <94167082+prosviriakova@users.noreply.github.com>
2024-12-08 23:18:14 +0000

    refactor(parser): fix norm, return value (unclosed quotes)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,41 +6,47 @@
 /*   By: oprosvir <oprosvir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 11:42:50 by oprosvir          #+#    #+#             */
-/*   Updated: 2024/12/07 15:32:05 by oprosvir         ###   ########.fr       */
+/*   Updated: 2024/12/08 23:16:49 by oprosvir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	current_token(t_token **tokens, t_command *current, t_shell *shell)
+static	int current_token(t_token **tokens, t_command *current, t_shell *shell)
 {
 	char	*exec_path;
 
 	if (is_redirect((*tokens)->type))
+		return (parse_redirects(tokens, current, shell));
+	parse_args(tokens, current);
+	if (current->name && !is_builtin(current->name))
 	{
-		if (parse_redirects(tokens, current, shell) == -1)
-			return (-1);
-	}
-	else
-	{
-		parse_args(tokens, current);
-		if (current->name && !is_builtin(current->name))
-		{
-			exec_path = get_path(current->name, shell->env_vars);
-			if (!exec_path)
-				return (err_msg(current->name, "command not found", shell, 127),
-					-1);
-			free(current->name);
-			current->name = exec_path;
-			free(current->args[0]);
-			current->args[0] = ft_strdup(current->name);
-		}
+		exec_path = get_path(current->name, shell->env_vars);
+		if (!exec_path)
+			return (err_msg(current->name, "command not found", shell, 127),
+				-1);
+		free(current->name);
+		current->name = exec_path;
+		free(current->args[0]);
+		current->args[0] = ft_strdup(current->name);
 	}
 	return (0);
 }
 
-// TODO : Function has more than 25 lines 
-t_command	*build_command(t_token *tokens, t_shell *shell)
+static t_command	*parse_command(t_token **tokens, t_shell *shell, t_command **current)
+{
+	if (!*current)
+	{
+		*current = init_command(shell);
+		if (!*current)
+			return NULL;
+	}
+	if (current_token(tokens, *current, shell) == -1)
+		return (NULL);
+	return *current;
+}
+
+static t_command	*build_command(t_token *tokens, t_shell *shell)
 {
 	t_command	*head;
 	t_command	*current;
@@ -56,16 +62,11 @@ t_command	*build_command(t_token *tokens, t_shell *shell)
 		}
 		else
 		{
+			current = parse_command(&tokens, shell, &current);
 			if (!current)
-			{
-				current = init_command(shell);
-				if (!current)
-					return (free_commands(head), NULL);
-				if (!head)
-					head = current;
-			}
-			if (current_token(&tokens, current, shell) == -1)
 				return (free_commands(head), NULL);
+			if (!head)
+				head = current;
 		}
 	}
 	return (head);
@@ -78,8 +79,8 @@ t_command	*parser(char *line, t_shell *shell)
 
 	tokens = lexer(line, shell);
 	if (!tokens)
-		return (set_status(shell, 1));
-	// print_tokens(tokens);
+		return (NULL);
+	//print_tokens(tokens);
 	head = build_command(tokens, shell);
 	free_tokens(tokens);
 	return (head);
